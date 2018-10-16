ba652251b13e67dcd3fa2358fdc29c25a2af1b45
paristuzun <paristuzun@gmail.com>
2018-10-16 01:22:56 -0400

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,12 @@
+#One Argument Method
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+
+#Two Argument Method
+
+def introduction_with_language(name, language)
+	puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
